54081bf6433342b8e7b2214ae66c525d9ceda600
Francisco53 <franciscolima2228648@gmail.com>
2023-03-20 16:04:58 -0300

    Projeto inicializado com a funcionalidade listar vendas

---
+++ src/main/java/com/francisco/ecommerce/ConfiguracaoSpringMvc.java
@@ -0,0 +1,19 @@
+package com.francisco.ecommerce;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfiguracaoSpringMvc implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+
+    }
+}
