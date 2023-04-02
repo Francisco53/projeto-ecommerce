d49db0a0f95056497dcc2c53811fe091c5882de9
Francisco53 <franciscolima2228648@gmail.com>
2023-04-02 14:06:34 -0300

    cadastro de produto

---
+++ src/main/java/com/francisco/ecommerce/controllers/ProdutoController.java
@@ -0,0 +1,62 @@
+package com.francisco.ecommerce.controllers;
+
+
+import com.francisco.ecommerce.entities.Produto;
+import com.francisco.ecommerce.respositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Transactional
+@Controller
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    @Autowired
+    ProdutoRepository repository;
+
+    @GetMapping("/list")
+    public String listar(Model model){
+        List<Produto> produtos = repository.produtos();
+        model.addAttribute("produtos", produtos);
+        return "/produtos/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Produto produto){
+        return "/produtos/form";
+    }
+
+    @PostMapping("/save")
+    public ModelAndView save(Produto produto){
+        repository.save(produto);
+        return new ModelAndView("redirect:/produtos/list");
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("produto", repository.produto(id));
+        return new ModelAndView("/produtos/form", model);
+    }
+
+    @PostMapping("/update")
+    public ModelAndView update(Produto produto) {
+        repository.update(produto);
+        return new ModelAndView("redirect:/produtos/list");
+    }
+
+    @GetMapping("/remove/{id}")
+    public ModelAndView remove(@PathVariable("id") Long id){
+        repository.remove(id);
+        return new ModelAndView("redirect:/produtos/list");
+    }
+}
